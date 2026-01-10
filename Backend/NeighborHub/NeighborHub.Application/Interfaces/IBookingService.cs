26be75954dc1038ea4714008589fe90733838eb8
JaredKishCodes <missionajk@gmail.com>
2026-01-10 18:27:38 +0800

    Added BookingService Interface

--- Backend/NeighborHub/NeighborHub.Application/Interfaces/IBookingService.cs
+++ Backend/NeighborHub/NeighborHub.Application/Interfaces/IBookingService.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeighborHub.Application.DTOs.Booking;
 
 namespace NeighborHub.Application.Interfaces;
 public interface IBookingService
 {
+    Task<List<BookingResponseDto>> GetMyBorrowingAsync(int userId);
+    Task<List<BookingResponseDto>> GetMyLendingAsync(int userId);
+    Task<BookingResponseDto> GetBookingByIdAsync(int bookingId);
+    Task<BookingResponseDto> CreateBooking(CreateBookingDto createBookingDto);
+    Task<BookingResponseDto> UpdateBooking(int bookingId, UpdateBookingDto updateBookingDto);
+    Task<bool> DeleteBooking(int bookingId);
 
 }
