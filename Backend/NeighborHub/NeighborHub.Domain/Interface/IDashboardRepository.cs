94c2ed6c386d09b90c47f42e1a0cefd9e7de1498
JaredKishCodes <missionajk@gmail.com>
2026-01-14 08:52:21 +0800

    Added Dashboard Repository Interface

---
+++ Backend/NeighborHub/NeighborHub.Domain/Interface/IDashboardRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeighborHub.Domain.Entities;
+
+namespace NeighborHub.Domain.Interface;
+public interface IDashboardRepository
+{
+    Task<int> GetTotalUsersCountAsync();
+    Task<int> GetTotalToolsCountAsync();
+    Task<int> GetTotalBookingsCountAsync();
+    Task<int> GetTotalLendingsCountAsync();
+    Task<int> GetTotalBookingsThisMonthAsync();
+    Task<IEnumerable<Booking>> GetBookingsForCurrentMonthAsync();
+    Task<IEnumerable<Booking>> GetLendingsForCurrentMonthAsync();
+
+
+}
